c9205b4cb84c3dc60ebe0348f4d7e5ec434f0205
Matthys <matthysk@gmail.com>
2016-03-15 18:17:39 +0200

    Make installable with setup.py

---
+++ excite/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(
+    name='django-sb',
+    version='0.0',
+    author = "Matthys Kroon",
+    author_email = "matthysk@clearspark.co.za",
+    description='Accounting app.',
+    license = "Proprietary, copyright ClearSpark (c) 2016",
+    packages=['sb'],
+    include_package_data=True,
+    install_requires=[
+    ]
+)
